f6d41046642e69fbd5b77eee555f0b1c5fe70fd7
Erik Claros <erikclaros.bus@gmail.com>
2024-09-20 15:45:55 -0500

    chore: Add initial project files and configurations

--- SoundApp/README.md
+++ SoundApp/README.md
@@ -1,6 +1,10 @@
 # SoundApp
 
+<<<<<<< HEAD
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.4.
+=======
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.2.
+>>>>>>> 0b095f1 (chore: Add initial project files and configurations)
 
 ## Development server
 
--- SoundApp/src/app/app.config.ts
+++ SoundApp/src/app/app.config.ts
@@ -2,7 +2,14 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+<<<<<<< HEAD
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+=======
+import { provideClientHydration } from '@angular/platform-browser';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+>>>>>>> 0b095f1 (chore: Add initial project files and configurations)
 };
