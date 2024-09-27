db5ed5f67e2f513d72b283d5abc6f4b65069a565
ethan <ethanenniscp0305@gmail.com>
2024-09-27 14:41:59 -0500

    current progress

---
+++ SoundApp/src/app/like-button/like-button.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-like-button',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      like-button works!
+    </p>
+  `,
+  styleUrl: './like-button.component.css'
+})
+export class LikeButtonComponent {
+
+}
