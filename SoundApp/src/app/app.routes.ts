26b0a8a7770e90dc4b22fdb2dfaca26056cba331
Dipesh Bikram Shah <dipeshshah070@gmail.com>
2024-09-27 14:28:20 -0500

    Created a dashboard page.

--- SoundApp/src/app/app.routes.ts
+++ SoundApp/src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';  // path to DashboardComponent
+
+export const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },  // Route for dashboard
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }  // Default page
+];
