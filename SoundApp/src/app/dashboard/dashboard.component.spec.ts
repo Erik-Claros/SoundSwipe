8be00b5d2a605ada11d70603984a146ad3549c01
Braden Nickel <banickel2016@gmail.com>
2024-09-30 12:44:38 -0500

    Add playlist button to dashboard component

--- SoundApp/src/app/dashboard/dashboard.component.spec.ts
+++ SoundApp/src/app/dashboard/dashboard.component.spec.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { PlaylistButtonComponent } from "../playlist-button/playlist-button.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,  // Ensure this is marked as standalone
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  imports: [PlaylistButtonComponent]
 })
 export class DashboardComponent {}
--- SoundApp/src/app/dashboard/dashboard.component.ts
+++ SoundApp/src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PlaylistButtonComponent } from '../playlist-button/playlist-button.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [PlaylistButtonComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']  // Correct the typo here
 })
