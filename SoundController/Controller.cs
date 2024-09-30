2614596bb755d0de00157e5f9b10112b5ac84b6a
Erik-Claros <erikclaros.bus@gmail.com>
2024-09-30 14:33:53 -0500

    Current Implementation For Spotify API

--- SoundController/Controller.cs
+++ SoundController/Controller.cs
@@ -1,21 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
+public class FileReader {
+    string filePath = "SoundController/Secrets-File/config.json";
+   public static object fileReader() {
+       string json = System.IO.File.ReadAllText(filePath);
+       JObject jObject = JObject.Parse(json);
+
+       console.WriteLine(jObject["clientID"]);
+       console.WriteLine(jObject["clientSecret"]);
+       console.WriteLine(jObject["scope"]); 
+       return jObject;
+   }    
+}
 namespace MyApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class MyController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Get()
-        {
-            return Ok("Hello from MyController!");
-        }
+        // public IActionResult login () {
+        //     string clientID = JsonReader.JsonReader()["clientID"];
+        //     string clientSecret = JsonReader.JsonReader()["clientSecret"];
 
-        [HttpGet("{id}")]
-        public IActionResult GetById(int id)
-        {
-            return Ok($"You requested item {id}");
-        }
+        //     string authURL = "https://accounts.spotify.com/authorize";
+        //     string redirectURI = "http://localhost:5000/callback";
+        //     return;
+        // }    
     }
 }
