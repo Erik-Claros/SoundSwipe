9dabd7ca189afba1f7c4538b3779d3e4ea889097
Braden Nickel <banickel2016@gmail.com>
2024-09-30 12:26:47 -0500

    The share button uses an image from google. The image is a circle and the button has been rounded to fit around the image, so clicking the image resembles a button. Some functionality has been added: it uses a Web Share API built into Angular (openShareMenu()) and the localhost and test supports this API.

--- SoundApp/src/app/dashboard/dashboard.component.spec.ts
+++ SoundApp/src/app/dashboard/dashboard.component.spec.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ShareButtonComponent } from "../share-button/share-button.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,  // Ensure this is marked as standalone
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  imports: [ShareButtonComponent]
 })
 export class DashboardComponent {}
