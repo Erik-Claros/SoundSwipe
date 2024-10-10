c30f52b22cd85fc010c8ea619865d01cafdb7228
ethan <ethanenniscp0305@gmail.com>
2024-10-09 20:29:59 -0500

    Got the friend drop down menu to work when json server is live

--- SoundController/obj/Debug/net8.0/SoundController.AssemblyInfo.cs
+++ SoundController/obj/Debug/net8.0/SoundController.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoundController")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+478d943a4d999ab97283a1d463e979a0d456547a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba9c47845f6a9de8bc00a93a3ac2a2d5b63e5865")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoundController")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoundController")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
