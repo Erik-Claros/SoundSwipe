c515bc510b31a243a4569532cdf2a89457ded92e
Braden Nickel <banickel2016@gmail.com>
2024-10-16 09:38:03 -0500

    Completed Navigation bar with menu dropdown

--- SoundApp/src/app/dashboard/dashboard.component.ts
+++ SoundApp/src/app/dashboard/dashboard.component.ts
@@ -4,11 +4,19 @@ import { LikeButtonComponent } from '../like-button/like-button.component';
 import { PlaylistButtonComponent } from '../playlist-button/playlist-button.component';
 import { ShareButtonComponent } from '../share-button/share-button.component';
 import { FriendButtonComponent } from "../friend-button/friend-button.component";
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [PlaylistButtonComponent, ShareButtonComponent, DislikeComponent, LikeButtonComponent, FriendButtonComponent ],
+  imports: [
+    PlaylistButtonComponent, 
+    ShareButtonComponent, 
+    DislikeComponent, 
+    LikeButtonComponent, 
+    FriendButtonComponent, 
+    NavBarComponent 
+  ],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 
---
+++ SoundApp/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule
+  ]
+})
+export class NavBarComponent {
+}
