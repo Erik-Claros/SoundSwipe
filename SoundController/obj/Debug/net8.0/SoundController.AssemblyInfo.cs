f1739779301be9fed4ef1a7683b9959d6de46114
Erik-Claros <erikclaros.bus@gmail.com>
2024-10-19 15:45:10 -0500

    Misc Files

--- SoundController/obj/Debug/net8.0/SoundController.AssemblyInfo.cs
+++ SoundController/obj/Debug/net8.0/SoundController.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoundController")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f0be1ecdd55fc878a78b8ab4af0decab4880205")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c71fc2188e33f71b91e41149dc4356e7154b081")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoundController")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoundController")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
