414408a369fbc468f6c69dcf53944f01677b77a4
Erik-Claros <erikclaros.bus@gmail.com>
2024-10-19 15:42:41 -0500

    Updated some assests

--- SoundController/obj/Debug/net8.0/SoundController.AssemblyInfo.cs
+++ SoundController/obj/Debug/net8.0/SoundController.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoundController")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63cbbe65d33fb6a22746d3400d3f5c66894be6b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd01261f1f8ef6c84a8846c87785f9c82350870d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoundController")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoundController")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
