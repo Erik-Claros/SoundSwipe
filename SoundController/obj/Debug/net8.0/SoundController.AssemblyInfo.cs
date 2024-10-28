fdff63aa3eb8cd346ade8a1c6093f164919bee65
ethan <ethanenniscp0305@gmail.com>
2024-10-28 09:39:00 -0500

    automatic controller changes

--- SoundController/obj/Debug/net8.0/SoundController.AssemblyInfo.cs
+++ SoundController/obj/Debug/net8.0/SoundController.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoundController")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36481945e091e1a133a466b92a13d3651c34b274")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b5264917057ab023c7bd34c1a53a28a482da97e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoundController")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoundController")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
