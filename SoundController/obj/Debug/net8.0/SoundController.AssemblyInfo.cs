5cabf1c7ad0c395dd46fb529e1ed715b22652757
Dipesh Bikram Shah <dipeshshah070@gmail.com>
2024-11-12 22:20:46 -0600

    soundcontroller

--- SoundController/obj/Debug/net8.0/SoundController.AssemblyInfo.cs
+++ SoundController/obj/Debug/net8.0/SoundController.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoundController")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d69ce8ab6bb480fbef3fa68157d943cb62d0040")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d688268e461c5e99c3a2fb4853ae32cf2be7fc43")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoundController")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoundController")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
